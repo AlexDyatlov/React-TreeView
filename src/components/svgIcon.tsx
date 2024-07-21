ac197dc702fa12edd891c04041c91913d22c8f03
AlexDyatlov <alexdyatlov.web@gmail.com>
2024-07-21 19:11:22 +0300

    Adds html layout

---
+++ src/components/svgIcon.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+interface SvgIconProps {
+  name: string;
+  size: string;
+  className?: string;
+}
+
+const SvgIcon: React.FC<SvgIconProps> = ({ name, size, className, ...props }) => {
+  return (
+    <svg className={clsx('shrink-0 fill-current', className)} width={size} height={size} {...props}>
+      <use xlinkHref={`/sprite.svg#${name}`} />
+    </svg>
+  );
+};
+
+export default SvgIcon;
